ea58c0ab4aa7f256dfded937b17fd832d7e1ec41
avraham <aviv4134@gmail.com>
2026-06-22 22:21:41 +0300

    project

--- week_11/day2/project tracks/tracks/tracks/Program.cs
+++ week_11/day2/project tracks/tracks/tracks/Program.cs
@@ -13,12 +13,58 @@ namespace main
             List<int> idList = new List<int>();
             List<int> speedList = new List<int>();
             List<int> headingList = new List<int>();
-            
+
 
             bool flag = true;
-            ShowMenu();
-            AddTruck(idList, speedList, headingList);
-            FindTruck(54, idList, speedList, headingList);
+            while (flag) 
+            {
+                ShowMenu();
+                string menuInput = Console.ReadLine();
+                switch (menuInput)
+                {
+                    case "1":
+                      AddTruck(idList, speedList, headingList);
+                        break;
+                    
+                    case "2":
+                        string inputId = Console.ReadLine();
+                        int id = int.Parse(inputId);
+                        RemoveTruckByID(id, idList, speedList, headingList);
+                        break;
+                    
+                    case "3":
+                        string inputId2 = Console.ReadLine();
+                        int id2 = int.Parse(inputId2);
+                        FindTruck(id2, idList, speedList, headingList);
+                        break;
+
+                    case "4":
+                        AllTrucks(idList, speedList, headingList);
+                        break;
+                    
+                    case "5":
+                        string inputSpeed = Console.ReadLine();
+                        int speed = int.Parse(inputSpeed);
+                        AllTrucks(speed, idList, speedList, headingList);
+                        break;
+                    
+                    case "6":
+                        Console.WriteLine(Summarize(idList, speedList, headingList));
+                        break;
+                    
+                    case "7":
+                        break;
+
+
+
+                }
+                
+            }
+            
+            
+            
+            //AddTruck(idList, speedList, headingList);
+            //FindTruck(54, idList, speedList, headingList);
             //AllTrucks(idList,speedList,headingList);
             //RemoveTruckByID(1,idList,speedList,headingList);
             //AllTrucks(idList, speedList, headingList);
@@ -30,6 +76,7 @@ namespace main
         static void ShowMenu()
         {
         Console.WriteLine("""
+            
             WELCOM TO TRUCKS SYSTEM
             
             MENU (1 - 7)
@@ -37,8 +84,11 @@ namespace main
             2:remove truck 
             3:find truck by id
             4:show all trucks
-            5:summerize trucks
-            6:quit 
+            5:filter trucks by speed
+            6:summerize trucks
+            7:quit 
+            
+            enter only 1 - 7 numbers:
             """);
         }
 
@@ -87,14 +137,17 @@ namespace main
         {
             for (int i = 0; i < idList.Count; i++)
             {
-                if (idList[i] == id) ; int index = i;
-
-                idList.RemoveAt(i);
-                speedList.RemoveAt(i);
-                headingList.RemoveAt(i);
-                break;
+                if (idList[i] == id)
+                {
+                    int index = i;
+                    idList.RemoveAt(i);
+                    speedList.RemoveAt(i);
+                    headingList.RemoveAt(i);
+                    Console.WriteLine("\nsuccess to remove");
+                    break;
+                }
             }
-            Console.WriteLine("\nsuccess to remove");
+            Console.WriteLine("\n id not found");
         }
 
         static void AllTrucks(List<int> idList, List<int> speedList, List<int> headingList)
@@ -120,6 +173,33 @@ namespace main
             }
             Console.WriteLine("id not found");
         }
+
+        static void AllTrucks(int speed, List<int> idList, List<int> speedList, List<int> headingList)
+        {
+            List<string> matchSpeed = new List<string>();
+            for (int i = 0; i < speedList.Count; i++)
+            {
+                if (speedList[i]>= speed)
+                {
+                    matchSpeed.Add($"id: {idList[i]} speed: {speedList[i]} heading: {headingList[i]}");
+                }
+                
+            }
+            foreach (string v in matchSpeed) Console.WriteLine(v);
+                
+        }
+
+        static string Summarize(List<int> idList, List<int> speedList, List<int> headingList)
+        {
+            int count = idList.Count;
+            int avrage = speedList.Sum() / count;
+            int maxSpeed = speedList.Max();
+            int indexFastest = speedList.IndexOf(maxSpeed);
+            string fastestTruck = $"id: {idList[indexFastest]} max speed: {speedList[indexFastest]} heading: {headingList[indexFastest]}"; 
+            return $"count: {count} avrage: {avrage}\nthe fastest truck is {fastestTruck}";
+        }
+
+        
 
 
 
