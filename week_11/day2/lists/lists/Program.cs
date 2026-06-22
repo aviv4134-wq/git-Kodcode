a7e0dd3f623ba5e1743838fe33d40d9b53c65f6b
avraham <aviv4134@gmail.com>
2026-06-22 12:16:00 +0300

    day 2

---
+++ week_11/day2/lists/lists/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection.Metadata;
+using System.Xml;
+
+namespace day2
+{
+    class lists
+    {
+        static List<string> tracks = new List<string>();
+        static List<double> speeds = new List<double>();
+
+        static void ADDTrack(string id, double speed)
+        {
+            tracks.Add(id);
+            speeds.Add(speed);
+
+        }
+        static double AvrageSpeed()
+        {
+            if (speeds.Count == 0) return 0.0;
+            double sum = 0;
+            foreach (double s in speeds) sum += s;
+            return sum/ speeds.Count;
+
+        }
+
+        static void Main()
+        {
+            ADDTrack("1", 100);
+            ADDTrack("10", 10);
+            Report("10");
+
+        }
+
+        static void Report(int id) // version 1: no parameters
+        
+        { 
+            Console.WriteLine($"{tracks.Count} tracks");
+        }
+        
+        static void Report(string id) // version 2: one string
+
+        {
+            int i = tracks.IndexOf(id);
+            if (i >= 0) Console.WriteLine($"{id}: {speeds[i]} kn");
+            else Console.WriteLine("id not fount");
+                     
+        }
+        
+               
+
+
+        
+    }
+}
